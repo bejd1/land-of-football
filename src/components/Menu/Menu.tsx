0e4c0ae4986bf56d8b13be05af062294474ca7d0
Filip <bejdi7@gmail.com>
2023-06-09 17:25:53 +0200

    .

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -14,7 +14,6 @@ export const Menu: React.FC<MenuProps> = ({ menuToggle }) => {
 
   const lockScroll = useCallback(() => {
     document.body.style.overflow = "hidden";
-    // document.body.style.paddingRight = "17px";
   }, []);
 
   lockScroll();
